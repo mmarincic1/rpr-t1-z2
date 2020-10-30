da91828c9b4896c89e21ef88fd915eb5ba54d805
Matej <mmarincic1@etf.unsa.ba>
2020-10-30 12:13:41 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/predavanje02/Main.java
@@ -0,0 +1,25 @@
+package ba.unsa.etf.rpr.predavanje02;
+
+import java.util.Scanner;
+
+public class Main {
+    private static int SumaCifara(int broj){
+        int suma = 0;
+        while(broj > 0){
+            suma+= broj%10;
+            broj/= 10;
+        }
+        return suma;
+    }
+    public static void main(String[] args) {
+        int n;
+        System.out.print("Unesi broj n: ");
+        Scanner ulaz = new Scanner(System.in);
+        n = ulaz.nextInt();
+        System.out.println("Brojevi koji su djeljivi sa sumom svojih cifara u opsegu od 1 do " + n +" su:");
+        for(int i = 1; i <= n; i++){
+            if(i % SumaCifara(i) == 0) System.out.print(i + " ");
+        }
+
+    }
+}
